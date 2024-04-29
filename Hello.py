f5da3e4360ff08ea95e0567a92f42941c888d573
Ethan Harstad <ethanharstad@gmail.com>
2024-04-29 19:22:25 +0000

    Connect to OpenAI

--- Hello.py
+++ Hello.py
@@ -3,37 +3,44 @@ from streamlit.logger import get_logger
 import numpy as np
 import random
 import time
+import os
+from openai import OpenAI
 
 LOGGER = get_logger(__name__)
+OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
 
-
-def response_generator():
-    response = random.choice([
-        "I am a pretty basic chat bot. How can I assist you today?",
-        "Hello human. Is there anything I can help you with?"
-    ])
-    for word in response.split():
-        yield word + " "
-        time.sleep(0.05)
+# Setup OpenAI model
+client = OpenAI(api_key=OPENAI_API_KEY)
+if "openai_model" not in st.session_state:
+    st.session_state["openai_model"] = "gpt-3.5-turbo"
 
 
 def run():
     st.title("Simple Bot")
 
+    # Setup chat history
     if "messages" not in st.session_state:
-        st.session_state.messages = []
-    
+        st.session_state.messages = []    
     for message in st.session_state.messages:
         with st.chat_message(message["role"]):
             st.markdown(message["content"])
     
+    # Accept user input
     if prompt := st.chat_input("What is up?"):
         with st.chat_message("user"):
             st.markdown(prompt)
         st.session_state.messages.append({"role": "user", "content": prompt})
 
         with st.chat_message("assistant"):
-            response = st.write_stream(response_generator())
+            stream = client.chat.completions.create(
+                model=st.session_state["openai_model"],
+                messages=[
+                    {"role": m["role"], "content": m["content"]}
+                    for m in st.session_state.messages
+                ],
+                stream=True,
+            )
+            response = st.write_stream(stream)
         st.session_state.messages.append({
             "role": "assistant",
             "content": response,
