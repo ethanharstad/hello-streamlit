18eec428327ccf067a20dfdf13641f3913f9a67a
Ethan Harstad <ethanharstad@gmail.com>
2024-04-29 03:59:42 +0000

    Basic chat control

--- Hello.py
+++ Hello.py
@@ -14,6 +14,7 @@
 
 import streamlit as st
 from streamlit.logger import get_logger
+import numpy as np
 
 LOGGER = get_logger(__name__)
 
@@ -24,27 +25,9 @@ def run():
         page_icon="👋",
     )
 
-    st.write("# Welcome to Streamlit! 👋")
-
-    st.sidebar.success("Select a demo above.")
-
-    st.markdown(
-        """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-    """
-    )
+    with st.chat_message("assistant"):
+        st.write("Hello Human")
+        st.bar_chart(np.random.randn(30, 3))
 
 
 if __name__ == "__main__":
